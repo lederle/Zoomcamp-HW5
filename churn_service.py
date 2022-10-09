8ee0854b429be3987e18229be3765c47e1c7f47c
mike lederle <mrlederle@gmail.com>
2022-10-09 18:37:56 -0400

    add churn prediction service in flask

--- churn_service.py
+++ churn_service.py
@@ -1,4 +1,20 @@
 import pickle
+from flask import Flask, request, jsonify
+
+app = Flask('churn')
+
+@app.route('/predict', methods = ['POST'])
+def predict():
+    client = request.get_json()
+    prediction = predict_client(client, dv, model)
+    churn = prediction >= 0.5
+
+    res = {
+        'churn_probability': float(prediction),
+        'churn': bool(churn)
+    }
+
+    return jsonify(res)
 
 with open('model1.bin', 'rb') as model_in:
     model = pickle.load(model_in)
@@ -8,7 +24,9 @@ with open('dv.bin', 'rb') as dv_in:
 
 client = {"reports": 0, "share": 0.001694, "expenditure": 0.12, "owner": "yes"}
 
-data = dv.transform([client])
-prediction = model.predict_proba(data)[0, 1]
-
-print(prediction)
+def predict_client(client, dict_vect, model):
+    data = dict_vect.transform([client])
+    return model.predict_proba(data)[0, 1]
+ 
+if __name__ == '__main__':
+    app.run(debug = True, host = '0.0.0.0', port = 9696)
